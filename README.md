7959fb72689df8c8bfa21a05e73800d1c23538a4
minnuss <ponjevic.goran@hotmail.com>
2021-05-01 16:22:49 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Todo List
+
+TITLE - Todo List
+
+About - Basic todo list with local storage, add, remove items
+
+TECHNOLOGIES USED:
+
+- HTML5
+- Google Fonts
+- Sass - Flexbox, Media Queries
+- JavaScript
+- Git
---
+++ js/main.js
@@ -0,0 +1,107 @@
+const form = document.querySelector('.form')
+const input = document.querySelector('.input')
+const todosBox = document.querySelector('.todos-list-box')
+const btnOk = document.querySelector('.btn')
+
+// READ FROM LOCAL STORAGE
+const todosLocal = JSON.parse(localStorage.getItem('todos'))
+if (todosLocal) {
+    todosLocal.forEach(todo => addTodo(todo));
+}
+
+// FORM ACTION
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    addTodo()
+})
+
+btnOk.addEventListener('click', () => {
+    e.preventDefault()
+    addTodo()
+})
+
+
+// CREATE TODO ELEMENT
+function addTodo(todo) {
+    let todoText = input.value
+
+    // reset input value after submit
+    input.value = ''
+
+    // if todo is read from local storage
+    if (todo) {
+        todoText = todo.text
+    }
+
+    // console.log(todoText)
+
+    if (todoText) {
+        const todoEl = document.createElement('li')
+
+        // if todo is read from local storage, update classlist
+        if (todo && todo.completed) {
+            todoEl.classList.add('completed')
+
+            // option to delete already completed todo from localstorage
+            let spanXlocalStorage = document.querySelectorAll('.completed span')
+            console.log(spanXlocalStorage)
+            spanXlocalStorage.forEach(x => {
+                x.addEventListener('click', (e) => {
+                    x.parentElement.remove()
+                })
+            })
+        }
+
+        // it needs to be innerHTML because of span element, if it exist in local storage, it needs to be recognised
+        todoEl.innerHTML = todoText
+
+        todoEl.addEventListener('click', () => {
+            todoEl.classList.toggle('completed')
+
+            // adding X / delete functionality to completed todo
+            if (todoEl.classList.contains('completed')) {
+                const spanX = document.createElement('span')
+                spanX.innerHTML = ' ❌'
+                todoEl.appendChild(spanX)
+
+                spanX.addEventListener('click', () => {
+                    spanX.parentElement.remove()
+                })
+            } else {
+                // delete every other span X and leave only first one
+                todoEl.querySelectorAll('span')[0].remove()
+            }
+
+            updateLocalStorage()
+        })
+
+        // adding delete functionality on mouse right click
+        todoEl.addEventListener('contextmenu', (e) => {
+            e.preventDefault()
+
+            todoEl.remove()
+            updateLocalStorage()
+        })
+
+        todosBox.appendChild(todoEl)
+
+        updateLocalStorage()
+    }
+}
+
+// ADD TO LOCAL STORAGE
+function updateLocalStorage() {
+    todosEl = document.querySelectorAll('li')
+
+    const todosArray = []
+
+    todosEl.forEach(todoEl => {
+
+        todosArray.push({
+            text: todoEl.innerHTML,
+            completed: todoEl.classList.contains('completed')
+        })
+    })
+
+    localStorage.setItem('todos', JSON.stringify(todosArray))
+}
